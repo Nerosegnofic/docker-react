bff1ae135610cbe7c858ee398f23d58ed42750ec
aabdelnabi <11410120220027@stud.cu.edu.eg>
2025-08-24 10:11:26 +0300

    first commit

---
+++ src/App.test.js
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Learn from Gamil/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+
